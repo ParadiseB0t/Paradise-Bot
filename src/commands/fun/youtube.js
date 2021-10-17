e7939b72157d446441a0338766617dd5c99981a7
DevCynx <71984728+DevCynx@users.noreply.github.com>
2021-10-17 15:59:13 +0100

    Delete youtube.js

--- src/commands/fun/youtube.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    category: 'Fun',
-    description: 'Replies with pong!',
-    slash: 'both',
-    testOnly: true,
-};
